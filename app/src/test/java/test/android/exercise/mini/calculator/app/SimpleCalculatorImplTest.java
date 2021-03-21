c6ef75183c0751300ec90fc092c56d3018e1ccbd
ronamir <ron.amir1@mail.huji.ac.il>
2021-03-21 18:14:14 +0200

    completed writing tests for SimpleCalculatorImpl and passed all

--- app/src/test/java/test/android/exercise/mini/calculator/app/SimpleCalculatorImplTest.java
+++ app/src/test/java/test/android/exercise/mini/calculator/app/SimpleCalculatorImplTest.java
@@ -47,12 +47,21 @@ public class SimpleCalculatorImplTest {
 
   @Test
   public void when_callingDeleteLast_then_lastOutputShouldBeDeleted(){
-    // todo: implement test
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(2);
+    calc.insertDigit(3);
+    calc.deleteLast();
+    assertEquals("2", calc.output());
+
   }
 
   @Test
   public void when_callingClear_then_outputShouldBeCleared(){
-    // todo: implement test
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(2);
+    calc.insertDigit(3);
+    calc.clear();
+    assertEquals("0", calc.output());
   }
 
   @Test
@@ -80,9 +89,186 @@ public class SimpleCalculatorImplTest {
   public void when_savingStateFromFirstCalculator_should_loadStateCorrectlyFromSecondCalculator(){
     SimpleCalculatorImpl firstCalculator = new SimpleCalculatorImpl();
     SimpleCalculatorImpl secondCalculator = new SimpleCalculatorImpl();
+    firstCalculator.insertDigit(5);
+    firstCalculator.insertDigit(6);
+    firstCalculator.insertPlus();
+    secondCalculator.insertDigit(7);
+    Serializable savedState = firstCalculator.saveState();
+    assertNotNull(savedState);
+    secondCalculator.loadState(savedState);
+    assertEquals("56+", secondCalculator.output());
+
+
     // TODO: implement the test based on this method's name.
     //  you can get inspiration from the test method `when_savingState_should_loadThatStateCorrectly()`
   }
+
+  @Test
+  public void complex_insertion_delete_last(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(5);
+    calc.insertPlus();
+    calc.insertDigit(1);
+    calc.insertDigit(7);
+    calc.insertMinus();
+    calc.insertDigit(1);
+    calc.insertDigit(3);
+    calc.deleteLast();
+    calc.insertDigit(2);
+    calc.insertDigit(5);
+    assertEquals("5+17-125", calc.output());
+  }
+
+  @Test
+  public void complex_insertion_clear(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(9);
+    calc.clear();
+    calc.insertDigit(1);
+    calc.insertDigit(2);
+    calc.clear();
+    calc.insertDigit(8);
+    calc.insertMinus();
+    calc.insertDigit(7);
+    calc.insertEquals();
+    assertEquals("1", calc.output());
+  }
+
+  @Test
+  public void multiple_equals(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(8);
+    calc.insertMinus();
+    calc.insertDigit(7);
+    calc.insertEquals();
+    calc.insertPlus();
+    calc.insertDigit(4);
+    calc.insertEquals();
+    calc.insertMinus();
+    calc.insertDigit(1);
+    calc.insertEquals();
+    assertEquals("4", calc.output());
+  }
+
+  @Test
+  public void check_negative_result(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(9);
+    calc.insertDigit(9);
+    calc.insertDigit(9);
+    calc.insertMinus();
+    calc.insertDigit(8);
+    calc.insertDigit(8);
+    calc.insertDigit(8);
+    calc.insertMinus();
+    calc.insertDigit(2);
+    calc.insertDigit(2);
+    calc.insertDigit(2);
+    calc.insertEquals();
+    calc.insertMinus();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    assertEquals("-111-333", calc.output());
+  }
+
+  @Test
+  public void big_numbers(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.insertPlus();
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertEquals();
+    assertEquals("9999", calc.output());
+  }
+
+  @Test
+  public void two_zeros(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(0);
+    assertEquals("0", calc.output());
+  }
+
+  @Test
+  public void long_complex_calc(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.clear();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.deleteLast();
+    calc.insertPlus();
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertEquals();
+    assertEquals("6669", calc.output());
+  }
+
+  @Test
+  public void negative_output(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(5);
+    calc.insertMinus();
+    calc.insertDigit(1);
+    calc.insertDigit(0);
+    calc.insertEquals();
+    assertEquals("-5", calc.output());
+  }
+
+  @Test
+  public void combine_all_buttons(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.clear();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.deleteLast();
+    calc.insertPlus();
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertMinus();
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertEquals();
+    assertEquals("3", calc.output());
+  }
+
+  @Test public void combine_all_buttons_and_save(){
+    SimpleCalculatorImpl calc = new SimpleCalculatorImpl();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.clear();
+    calc.insertDigit(3);
+    calc.insertDigit(3);
+    calc.deleteLast();
+    calc.insertPlus();
+    // save current state
+    Serializable savedState = calc.saveState();
+    assertNotNull(savedState);
+    calc.clear();
+    assertEquals("0", calc.output());
+    calc.loadState(savedState);
+    assertEquals("3+", calc.output());
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertMinus();
+    calc.insertDigit(6);
+    calc.insertDigit(6);
+    calc.insertEquals();
+    assertEquals("3", calc.output());
+  }
+
+
 
   // TODO:
   //  the existing tests are not enough since they only test simple use-cases with small inputs.
